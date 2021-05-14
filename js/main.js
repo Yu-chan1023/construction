b2d2e30c450ac00c35cbfe673212991d1fd2147c
Yu-chan1023 <minami.15801580@gmail.com>
2021-05-14 14:58:15 +0900

    アニメーション作成

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 $(function() {
   let open = false;
+  let menuNav
   let btn = document.getElementById('menu');
   btn.addEventListener('click', () => {
     if (open === false) {
       $('.menu div').addClass('open').removeClass('close');
-      $('.menu-nav').addClass('open').removeClass('close');
-      // $('.menu div').removeClass('close');
+      menuNav = new TimelineMax();
+      menuNav.addLabel('nav')
+             .to('.menu-nav', 0.5, {opacity: 1, pointerEvents: 'auto', ease: Power2.easeOut}, 'nav')
+             .staggerTo('.menu-nav ul li', 1.6, {opacity: 1, x: 0, ease: Power2.easeOut}, 0.2, 'nav+=.7')
     } else {
       $('.menu div').removeClass('open').addClass('close');
-      $('.menu-nav').removeClass('open');
-      // $('.menu div').addClass('close');
+      menuNav.addLabel('nav')
+             .to('.menu-nav', 0.5, {opacity: 0, pointerEvents: 'none', ease: Power2.easeOut}, 'nav')
     }
     open =! open
-  })
+  }) 
+
 })
